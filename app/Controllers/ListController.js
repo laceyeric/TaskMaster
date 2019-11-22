6bee4b4af0cfbd981ace219c86c5e406999ac431
laceyeric <elaceyinfo@yahoo.com>
2019-11-22 15:00:30 -0700

    create to controller and service connected

--- app/Controllers/ListController.js
+++ app/Controllers/ListController.js
@@ -1,7 +1,11 @@
 import ListService from "../Services/ListService.js";
+import store from "../store.js";
 
 //TODO Don't forget to render to the screen after every data change.
-function _drawLists() {}
+function _drawLists() {
+  console.log(store.Lists);
+
+}
 
 //Public
 export default class ListController {
@@ -11,4 +15,16 @@ export default class ListController {
   }
 
   //TODO: Your app will need the ability to create, and delete both lists and listItems
+
+  addList() {
+    event.preventDefault();
+    console.log("add list hits the controller");
+    // TODO get this data from form submission
+    let btnList = {
+      name: "fakeList",
+      items: []
+    }
+    ListService.addList(btnList);
+    _drawLists();
+  }
 }
--- app/Services/ListService.js
+++ app/Services/ListService.js
@@ -1,4 +1,5 @@
 import List from "../Models/List.js";
+import store from "../store.js";
 
 //Public
 class ListService {
@@ -6,6 +7,14 @@ class ListService {
   //given the information you need in the controller,
   //what methods will you need to do when this class is first 'constructed'?
   //NOTE You will need this code to persist your data into local storage, be sure to call the store method to save after each change
+
+  addList(rawdata) {
+    console.log("Did we get to the Service?");
+    let entry = new List(rawdata);
+
+    store.State.lists.push(entry);
+    store.saveState();
+  }
 }
 
 const SERVICE = new ListService();
