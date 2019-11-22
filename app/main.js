c7dd80f8ab414dd5e6d8b1b69d001233d603b580
laceyeric <elaceyinfo@yahoo.com>
2019-11-22 11:37:02 -0700

    initial commit from fork

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ import ListController from "./Controllers/ListController.js";
 //NOTE This should be good to go
 class App {
   constructor() {
-    listController: new ListController();
+    this.listController = new ListController();
   }
 }
 
